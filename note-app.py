9f55f3d01015b3ea637258045982efd13b28aa3f
KirillBels <k.belskiy@icloud.com>
2025-01-17 21:59:06 +0300

    note-app

--- note-app.py
+++ note-app.py
@@ -25,57 +25,71 @@ def create_note():
 
 
 def read_note():
-    note_name_search = input('Введите название заметки которую хотите посмотреть!')
-    if os.path.isfile(f'list_note/{note_name_search}.txt'):
-        try:
-            with open(f'list_note/{note_name_search}.txt', 'r', encoding='utf-8') as file:
-                for el in file:
-                    print(el)
-        except Exception:
-            print('Произошла ошибка, попробуйте еще раз!')
+    try:
+        note_name_search = input('Введите название заметки которую хотите посмотреть!')
+        if os.path.isfile(f'list_note/{note_name_search}.txt'):
+            try:
+                with open(f'list_note/{note_name_search}.txt', 'r', encoding='utf-8') as file:
+                    for el in file:
+                        print(el)
+            except Exception:
+                print('Произошла ошибка, попробуйте еще раз!')
 
-    elif not os.path.isfile(f'list_note/{note_name_search}'):
-        print('Заметка не найдена')
+        elif not os.path.isfile(f'list_note/{note_name_search}'):
+            print('Заметка не найдена')
+    except Exception:
+        print('Произошла ошибка! Поробуйте еще раз!')
 
 
 def edit_note():
-    note_name_edit = input('Введите название заметки которую хотите редактировать')
-    if os.path.isfile(f'list_note/{note_name_edit}.txt'):
-        with open(f'list_note/{note_name_edit}.txt', 'r', encoding='utf-8') as file:
-            files = file.read()
-            print(files)
-
-        print('Что хотите сделать с заметкой?\n1.Добавить текст в конец\n2.Добавить полностью новый текст')
-        choice = int(input('Ваш выбор: '))
-        if choice == 1:
+    try:
+        note_name_edit = input('Введите название заметки которую хотите редактировать')
+        if os.path.isfile(f'list_note/{note_name_edit}.txt'):
             try:
-                text = input('Введите текст: ')
-                with open(f'list_note/{note_name_edit}.txt', 'a', encoding='utf-8') as file:
-                    file.write(text)
-                    print('Текст добавлен')
-            except ValueError:
-                print('Ошибка!')
+                with open(f'list_note/{note_name_edit}.txt', 'r', encoding='utf-8') as file:
+                    files = file.read()
+                    print(files)
+                try:
+                    print('Что хотите сделать с заметкой?\n1.Добавить текст в конец\n2.Добавить полностью новый текст')
+                    choice = int(input('Ваш выбор: '))
+                    if choice == 1:
+                        try:
+                            text = input('Введите текст: ')
+                            with open(f'list_note/{note_name_edit}.txt', 'a', encoding='utf-8') as file:
+                                file.write(text)
+                                print('Заметка обновлена')
+                        except ValueError:
+                            print('Ошибка!')
 
-        elif choice == 2:
-            try:
-                text = input('Введите текст: ')
-                with open(f'list_note/{note_name_edit}.txt', 'w', encoding='utf-8') as file:
-                    file.write(text)
-                    print('Текст добавлен')
-            except ValueError:
-                print('Ошибка!')
+                    elif choice == 2:
+                        try:
+                            text = input('Введите текст: ')
+                            with open(f'list_note/{note_name_edit}.txt', 'w', encoding='utf-8') as file:
+                                file.write(text)
+                                print('Заметка обновлена')
+                        except ValueError:
+                            print('Ошибка!')
+                except Exception:
+                    print('Произошла ошибка! Попробуйте еще раз!')
+            except Exception:
+                print('Произошла ошибка! Попробуйте еще раз!')
+    except Exception:
+        print('Произошла ошибка! Попробуйте еще раз!')
 
 
 def delete_note():
-    name_delete_note: str = input('\nВведите название заметки: ')
-    if os.path.isfile(f'list_note/{name_delete_note}.txt'):
-        try:
-            os.remove(f'list_note/{name_delete_note}.txt')
-            print(f'\nЗаметка "{name_delete_note}", удалена!\n')
-        except Exception:
-            print('Произошла ошибка!')
-    elif not os.path.isfile(f'list_note/{name_delete_note}.txt'):
-        print('\nТакой заметки не существует\n')
+    try:
+        name_delete_note: str = input('\nВведите название заметки: ')
+        if os.path.isfile(f'list_note/{name_delete_note}.txt'):
+            try:
+                os.remove(f'list_note/{name_delete_note}.txt')
+                print(f'\nЗаметка "{name_delete_note}", удалена!\n')
+            except Exception:
+                print('Произошла ошибка!')
+        elif not os.path.isfile(f'list_note/{name_delete_note}.txt'):
+            print('\nТакой заметки не существует\n')
+    except Exception:
+        print('Произошла ошибка, попробуйте еще раз!')
 
 
 def display_notes():
@@ -127,6 +141,12 @@ def main():
                 print('\nТакого варианта не существует\n')
         except ValueError:
             print('\nОшибка, попробуйте еще раз!\n')
+
+        print("Чтобы продолжить работать с заметками, нажмите y/n")
+
+        answer = input().lower()
+        if answer != "y":
+            break
 
 
 main()
